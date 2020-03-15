ecf6f7ecfad3a9c3d3761c01d6fdaebd3dd5d41e
Alex5678910 <47632342+Alex5678910@users.noreply.github.com>
2020-03-15 17:56:12 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let plus = document.querySelector('.plus');
+let followers = document.querySelector('span.followers');
+let minus = document.querySelector('.minus');
+
+
+plus.addEventListener('click', func);
+
+function func() {
+    return followers.innerHTML++;
+}
+
+minus.addEventListener('click', fun);
+
+function fun() {
+    return followers.innerHTML--;
+}
+
+/*==========================Слайдер================================*/
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
